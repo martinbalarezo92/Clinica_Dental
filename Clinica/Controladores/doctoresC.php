cf5789dcdfee9a6f94a142f9b2e6bf0e8677c747
Ricardo Leon <martinbalarezo92@hotmail.com>
2020-07-10 20:15:01 -0500

    clase finalizada 44. Editar Perfil Doctor

--- Clinica/Controladores/doctoresC.php
+++ Clinica/Controladores/doctoresC.php
@@ -209,4 +209,97 @@ public function IngresarDoctorC(){
 
 	}
 
+	//Editar 
+
+	public function EditarPerfilDoctor(){
+
+		$tablaBD = "doctores";
+		$id = $_SESSION["id"];
+
+		$resultado = DoctoresM::VerPerfilDoctorM($tablaBD, $id);
+
+		echo'
+
+		<form method="post" enctype="multipart/form-data">
+					
+					<div class="row">
+						
+						<div class="col-md-6 col-xs-12">
+							
+							<h2>Nombre: </h2>
+							<input type="text" class="input-lg" name="nombrePerfil" value="'.$resultado["nombre"].'">
+							<input type="hidden" name="Did" value="'.$resultado["id"].'">
+
+							<h2>Apellido: </h2>
+							<input type="text" class="input-lg" name="apellidoPerfil" value="'.$resultado["apellido"].'">
+
+							<h2>Usuario: </h2>
+							<input type="text" class="input-lg" name="usuarioPerfil" value="'.$resultado["usuario"].'">
+
+							<h2>Clave: </h2>
+							<input type="text" class="input-lg" name="clavePerfil" value="'.$resultado["clave"].'">';
+
+
+							$columna = "id";
+
+					 		$valor = $resultado["id_consultorio"];
+
+					 		$consultorio = ConsultoriosC::VerConsultoriosC($columna, $valor);
+
+							echo'
+
+							<h2>Consultorio Actual: '.$consultorio["nombre"].'</h2>
+
+							<h3>Cambiar consultorio</h3>
+							<select class="input-lg" name= "consultorioPerfil">';
+								
+							$columna = null;
+							$valor = null;
+
+							$consultorio = ConsultoriosC::VerConsultoriosC($columna, $valor);
+
+							foreach ($consultorio as $key => $value) {
+								
+								echo'
+
+								<option value="'.$value["id"].'">'.$value["nombre"].'</option>
+
+								';
+
+							}	
+							echo'
+							</select>
+
+							
+							<div class="form-group">
+								
+								<h2>Horario:</h2>
+								Desde: <input type="time" class="input-lg" name="hePerfil" value="'.$resultado["horarioE"].'">
+								Hasta: <input type="time" class="input-lg" name="hsPerfil" value="'.$resultado["horarioS"].'">
+
+							</div>
+							
+						</div>
+
+						<div class="col-md-6 col-xs-12">
+							
+							<br><br>
+							<input type="file" name="imgPerfil">
+							<br>
+							<img src="http://localhost/clinica/Vistas/img/defecto.png" class="img-responsive" width="200px">
+
+							<input type="hidden" name="imgActual" value="">
+							<br><br>
+
+							<button type="submit" class="btn btn-success">Guardar Cambios</button>
+
+						</div>
+
+					</div>
+
+				</form>	
+
+		';
+
+	}
 }
